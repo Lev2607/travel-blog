2ba500239249ace8352083251f0473e8bbf23106
Lev2607 <levent.o@web.de>
2024-03-19 10:23:40 +0100

    feat: main.tf updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,93 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c574c8"
+# IAM Rolle für EC2 Instanz mit Berechtigungen 
+resource "aws_iam_role" "ec2_role" {
+  name = "travelBlog_ec2_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# Policy zur IAM Rolle, um auf S3 und DynamoDB zugreifen zu können
+
+resource "aws_iam_policy_attachment" "s3_policy_attachment" {
+  name       = "travelBlog_s3_policy_attachment"
+  roles      = [aws_iam_role.ec2_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_policy_attachment" "dynamodb_policy_attachment" {
+  name       = "travelBlog_dynamodb_policy_attachment"
+  roles      = [aws_iam_role.ec2_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+# EC2 Instanz mit IAM Rolle
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_role.ec2_role.name
 
   tags = {
-    Name = "example-instance"
+    Name = "travelBlogInstance"
+  }
+}
+
+# S3 Bucket
+
+resource "aws_s3_bucket" "image_bucket" {
+  bucket = "travelblog-images"
+
+  tags = {
+    Name = "travelBlogImageBucket"
+  }
+}
+
+resource "aws_s3_bucket_policy" "image_bucket_policy" {
+  bucket = aws_s3_bucket.image_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
+        ]
+        Resource = [
+          "${aws_s3_bucket.image_bucket.arn}/*",
+        ]
+      },
+    ]
+  })
+}
+
+# DynamoDB Table für die Kommentare
+
+resource "aws_dynamodb_table" "comments_table" {
+  name           = "travelBlogComments"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+  attribute {
+    name = "id"
+    type = "S"
+  }
+  tags = {
+    Name = "travelBlogCommentsTable"
   }
 }
