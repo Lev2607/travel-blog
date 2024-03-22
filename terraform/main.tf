e619942d29ad2c2b7ed67cf4014432295e445009
Lev2607 <levent.o@web.de>
2024-03-22 20:07:51 +0100

    api-gateway & lambda added to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -119,3 +119,163 @@ resource "aws_dynamodb_table" "comments_table" {
     Name = "travelBlogCommentsTable"
   }
 }
+
+# Erstellung der Lambda-Rolle
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_execution_role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Berechtigung für die Lambda-Funktion, um auf DynamoDB zuzugreifen
+resource "aws_iam_policy" "lambda_dynamodb_policy" {
+  name        = "lambda_dynamodb_policy"
+  description = "Policy for accessing DynamoDB from Lambda"
+
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Action    = [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem"
+      ],
+      Resource  = "*"
+    }]
+  })
+}
+
+# Anfügen der DynamoDB-Berechtigung an die Lambda-Rolle
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
+}
+
+# Erstellung der Lambda-Funktion "SaveCommentToDynamoDB"
+resource "aws_lambda_function" "save_comment_lambda" {
+  function_name    = "SaveCommentToDynamoDB"
+  handler          = "index.handler"
+  runtime          = "nodejs20.x"
+  role             = aws_iam_role.lambda_role.arn
+  filename         = "/home/lev/travel-blog/lambda/SaveComment.zip"  # Dateipfad zu Ihrer Lambda-ZIP-Datei
+  source_code_hash = filebase64sha256("/home/lev/travel-blog/lambda/SaveComment.zip")
+}
+
+# Erstellung der Lambda-Funktion "GetCommentsFromDynamoDB"
+resource "aws_lambda_function" "get_comments_lambda" {
+  function_name    = "GetCommentsFromDynamoDB"
+  handler          = "index.handler"
+  runtime          = "nodejs20.x"
+  role             = aws_iam_role.lambda_role.arn
+  filename         = "/home/lev/travel-blog/lambda/GetComment.zip"  # Dateipfad zu Ihrer Lambda-ZIP-Datei
+  source_code_hash = filebase64sha256("/home/lev/travel-blog/lambda/GetComment.zip")
+}
+
+# Erstellung der API-Gateway-Ressourcen
+
+resource "aws_api_gateway_rest_api" "comments_api" {
+  name        = "comments_api"
+  description = "API for managing comments"
+}
+
+resource "aws_api_gateway_resource" "comments_resource" {
+  rest_api_id = aws_api_gateway_rest_api.comments_api.id
+  parent_id   = aws_api_gateway_rest_api.comments_api.root_resource_id
+  path_part   = "comments"
+}
+
+# Erstellung der GET-Methode für das API-Gateway
+resource "aws_api_gateway_method" "get_comments_method" {
+  rest_api_id   = aws_api_gateway_rest_api.comments_api.id
+  resource_id   = aws_api_gateway_resource.comments_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+# Erstellung der Integration für die GET-Methode
+resource "aws_api_gateway_integration" "get_comments_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.comments_api.id
+  resource_id             = aws_api_gateway_resource.comments_resource.id
+  http_method             = "GET"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.get_comments_lambda.invoke_arn
+}
+
+# Erstellung der Methodenantwort für die GET-Methode
+resource "aws_api_gateway_method_response" "get_comments_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.comments_api.id
+  resource_id = aws_api_gateway_resource.comments_resource.id
+  http_method = aws_api_gateway_method.get_comments_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+# Erstellung der Integrationsantwort für die GET-Methode
+resource "aws_api_gateway_integration_response" "get_comments_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.comments_api.id
+  resource_id = aws_api_gateway_resource.comments_resource.id
+  http_method = "GET"  # Überprüfen Sie, ob dies mit Ihrer API-Konfiguration übereinstimmt
+  status_code = "200"  # Überprüfen Sie den Statuscode entsprechend Ihrer Anwendung
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'http://3.120.187.136'"
+  }
+}
+resource "aws_api_gateway_method" "comments_put_method" {
+  rest_api_id   = aws_api_gateway_rest_api.comments_api.id
+  resource_id   = aws_api_gateway_resource.comments_resource.id
+  http_method   = "PUT"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_method_response" "comments_put_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.comments_api.id
+  resource_id = aws_api_gateway_resource.comments_resource.id
+  http_method = aws_api_gateway_method.comments_put_method.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "comments_put_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.comments_api.id
+  resource_id             = aws_api_gateway_resource.comments_resource.id
+  http_method             = "PUT"
+  integration_http_method = "POST"  # Sie können dies an Ihre Anforderungen anpassen
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:eu-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:eu-central-1:346024832365:function:SaveCommentToDynamoDB/invocations"
+}
+
+resource "aws_api_gateway_integration_response" "comments_put_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.comments_api.id
+  resource_id = aws_api_gateway_resource.comments_resource.id
+  http_method = aws_api_gateway_method.comments_put_method.http_method
+  status_code = aws_api_gateway_method_response.comments_put_method_response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'http://3.120.187.136'"
+  }
+}
+
