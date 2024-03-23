b3e0d0d18815b3dc0555f5b2a956eec50a580544
Lev2607 <levent.o@web.de>
2024-03-23 21:25:54 +0100

    fixed index.js

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import styles from "./page.module.css";
 import axios from "axios";
 
 // Definieren Sie die API-Endpunkte für das Laden und Senden von Kommentaren
-const GET_COMMENTS_URL = "https://geexwt3fp3.execute-api.eu-central-1.amazonaws.com/comments";
-const POST_COMMENT_URL = "https://geexwt3fp3.execute-api.eu-central-1.amazonaws.com/comments";
+const GET_COMMENTS_URL = "https://wcsq6x0vs1.execute-api.eu-central-1.amazonaws.com/prod/comments";
+const POST_COMMENT_URL = "https://wcsq6x0vs1.execute-api.eu-central-1.amazonaws.com/prod/comments";
 
 export default function Home() {
   const [comments, setComments] = useState([]);
